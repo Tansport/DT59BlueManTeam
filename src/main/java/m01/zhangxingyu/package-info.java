a7da4a9316bca53835e66fa27e9e6c056fb9a3c6
lenovo <lenovo@DESKTOP-TAO>
2018-01-10 12:01:48 +0800

    add

---
+++ src/main/java/m01/zhangxingyu/package-info.java
@@ -0,0 +1,15 @@
+/**
+ * Project Name:DT59BlueManTeam
+ * File Name:package-info.java
+ * Package Name:m01.zhangxingyu
+ * Date:2018年1月10日下午12:01:27
+ * Copyright (c) 2018, bluemobi All Rights Reserved.
+ */
+/**
+ * Description: <br/>
+ * date: 2018年1月10日 下午12:01:27 <br/>
+ *
+ * @author liutao
+ * @version 
+ */
+package m01.zhangxingyu;
