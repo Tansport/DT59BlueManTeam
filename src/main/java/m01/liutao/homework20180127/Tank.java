3269fff7c3e1c451145bf4e3f3df63a0a17ab1e6
Administrator <Administrator@PC-201510181541>
2018-01-28 00:46:05 +0800

    Tank

---
+++ src/main/java/m01/liutao/homework20180127/Tank.java
@@ -0,0 +1,42 @@
+/**
+ * Project Name:study
+ * File Name:Tank.java
+ * Package Name:hight
+ * Date:2018年1月27日下午9:35:09
+ * Copyright (c) 2018, bluemobi All Rights Reserved.
+ */
+
+package m01.liutao.homework20180127;
+/**
+ * Description:   <br/>
+ * Date:     2018年1月27日 下午9:35:09 <br/>
+ * @author   liutao
+ * @version
+ * @see
+ */
+public class Tank {
+    int x;
+    int y;
+    public Tank() {
+        
+    }
+    public Tank(int x,int y) {
+        this.x=x;
+        this.y=y;
+    }
+    public int getX() {
+        return x;
+    }
+    public void setX(int x) {
+        this.x = x;
+    }
+    public int getY() {
+        return y;
+    }
+    public void setY(int y) {
+        this.y = y;
+    }
+    
+
+}
+
