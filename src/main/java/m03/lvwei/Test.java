e06c83c7477473b58d081cec6d599b7985b2e325
lw19950915 <435556279@qq.com>
2018-03-27 15:07:39 +0800

    lvwei

---
+++ src/main/java/m03/lvwei/Test.java
@@ -0,0 +1,76 @@
+/**
+ * Project Name:workspace1
+ * File Name:Test.java
+ * Package Name:lession180326
+ * Date:2018年3月27日下午2:38:16
+ * Copyright (c) 2018, bluemobi All Rights Reserved.
+ */
+
+package lession180326;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Description: <br/>
+ * Date: 2018年3月27日 下午2:38:16 <br/>
+ * 
+ * @author lvwei
+ * @version
+ * @see
+ */
+public class Test {
+
+    public static void main(String[] args) {
+
+        Test tt = new Test();
+        List<province> list = tt.getAll();
+        for (province ff : list) {
+            System.out.println(ff.getCid() + "\t" + ff.getCname() + "\t" + ff.getPname());
+        }
+
+    }
+
+    Connection con = null;
+
+    PreparedStatement pst = null;
+
+    ResultSet rs = null;
+
+    private String url = "jdbc:mysql://localhost:3306/mydata";
+
+    public Connection getCon() {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            con = DriverManager.getConnection(url, "root", "root");
+        } catch (Exception e) {
+            // TODO: handle exception
+            e.printStackTrace();
+        }
+        return con;
+    }
+
+    public List<province> getAll() {
+        List<province> list = new ArrayList<province>();
+        try {
+            String sql = "SELECT b.`cid`,b.`cname`,a.`pname` FROM province a INNER JOIN city b ON a.`pid`=b.`pid`";
+            pst = getCon().prepareStatement(sql);
+            rs = pst.executeQuery();
+            while (rs.next()) {
+                province ff = new province();
+                ff.setCid(rs.getInt("cid"));
+                ff.setCname(rs.getString("cname"));
+                ff.setPname(rs.getString("pname"));
+                list.add(ff);
+            }
+        } catch (Exception e) {
+            // TODO: handle exception
+            e.printStackTrace();
+        }
+        return list;
+    }
+}
---
+++ src/main/java/m03/lvwei/province.java
@@ -0,0 +1,49 @@
+/**
+ * Project Name:workspace1
+ * File Name:province.java
+ * Package Name:lession180326
+ * Date:2018年3月27日下午2:41:31
+ * Copyright (c) 2018, bluemobi All Rights Reserved.
+ */
+
+package lession180326;
+
+/**
+ * Description: <br/>
+ * Date: 2018年3月27日 下午2:41:31 <br/>
+ * 
+ * @author lvwei
+ * @version
+ * @see
+ */
+public class province {
+    private int cid;
+
+    private String cname;
+
+    private String pname;
+
+    public int getCid() {
+        return cid;
+    }
+
+    public void setCid(int cid) {
+        this.cid = cid;
+    }
+
+    public String getCname() {
+        return cname;
+    }
+
+    public void setCname(String cname) {
+        this.cname = cname;
+    }
+
+    public String getPname() {
+        return pname;
+    }
+
+    public void setPname(String pname) {
+        this.pname = pname;
+    }
+}
