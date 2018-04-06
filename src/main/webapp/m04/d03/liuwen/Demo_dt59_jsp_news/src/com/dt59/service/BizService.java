cb3e943252f9a04289cea43ed221375cc8bf0997
Tody_liu <574294003@qq.com>
2018-04-06 22:25:41 +0800

    liuwen

---
+++ src/main/webapp/m04/d03/liuwen/Demo_dt59_jsp_news/src/com/dt59/service/BizService.java
@@ -0,0 +1,23 @@
+/**
+ * Project Name:Demo_dt59_jsp_news
+ * File Name:BizService.java
+ * Package Name:com.dt59.service
+ * Date:2018年4月3日下午3:28:51
+ * Copyright (c) 2018, bluemobi All Rights Reserved.
+ */
+
+package com.dt59.service;
+
+import com.dt59.dao.BizDao;
+
+/**
+ * Description: <br/>
+ * Date: 2018年4月3日 下午3:28:51 <br/>
+ * 
+ * @author WEN LIU
+ * @version
+ * @see
+ */
+public interface BizService extends BizDao {
+
+}
