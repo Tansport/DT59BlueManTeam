586964df97b068585f8b40e13f09b8267d4d8721
liutao <lt0907@163.com>
2018-04-13 01:26:54 +0800

    liutao

---
+++ src/main/webapp/m04/d12/liutao/InitServlet.java
@@ -0,0 +1,83 @@
+package com.servlet.controller;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.Servlet;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.servlet.basedao.impl.BizDaoImpl;
+import com.servlet.entity.News;
+import com.servlet.entity.Product;
+import com.servlet.service.impl.BizServiceImpl;
+
+/**
+ * Servlet implementation class InitServlet
+ */
+public class InitServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public InitServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+    /**
+     * @see Servlet#init(ServletConfig)
+     */
+    public void init(ServletConfig config) throws ServletException {
+        // TODO Auto-generated method stub
+    }
+
+    /**
+     * @see Servlet#destroy()
+     */
+    public void destroy() {
+        // TODO Auto-generated method stub
+    }
+
+    /**
+     * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse
+     *      response)
+     */
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException,
+            IOException {
+        // TODO Auto-generated method stub
+    }
+
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+     *      response)
+     */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // TODO Auto-generated method stub
+        doPost(request, response);
+    }
+
+    /**
+     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+     *      response)
+     */
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException,
+            IOException {
+        // TODO Auto-generated method stub
+        BizDaoImpl bdi = new BizDaoImpl();
+        BizServiceImpl bsi = new BizServiceImpl();
+        bsi.setBizdao(bdi);// 为什么要注入
+        List<News> listnews = bsi.getAllNews();
+        HttpSession session = request.getSession();
+        session.setAttribute("listnews", listnews);
+        List<Product> listpro = bsi.getAllProduct();
+        session.setAttribute("listpro", listpro);
+        request.getRequestDispatcher("Jsp_Proscenium/M_Index.jsp").forward(request, response);
+    }
+
+}
